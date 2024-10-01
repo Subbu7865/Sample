b599fe70994ff6512b9b9d07197f9a8c810f63c6
Subbu7865 <123193289+Subbu7865@users.noreply.github.com>
2024-10-01 10:46:45 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Addition{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the First number:");
+    int a=sc.nextInt();
+    System.out.println("Enter the Second the number:");
+    int b=sc.nextInt();
+    int c=a+b;
+    System.out.println("The Addition of two numbers is:"+c);
+
+    } 
+     
+}
